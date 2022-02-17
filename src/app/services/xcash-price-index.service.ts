3c19c69418238f847224d7ec85e68c5079e32923
xcash-rocks <dev@xcash.rocks>
2022-02-17 15:36:51 +0100

    fix: rxjs/Rx to rxjs

--- src/app/services/xcash-price-index.service.ts
+++ src/app/services/xcash-price-index.service.ts
@@ -1,25 +1,25 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs/Rx';
+import { Observable } from 'rxjs';
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class XcashPriceIndexService {
-  
+
   constructor(private http: HttpClient) { }
-  
-  reqOptions = {               
+
+  reqOptions = {
     headers: {
       'Cache-Control':  'no-cache, no-store, must-revalidate, post-check=0, pre-check=0',
       'Pragma': 'no-cache',
       'Expires': '0'
     },
   };
-  
+
   reqUrl: string = 'https://api.coingecko.com/api/v3/simple/price?ids=x-cash&vs_currencies=usd&include_last_updated_at=true';
-  
+
   // For the moment this service is really simple and only uses coingecko API
   /**
    * Get the XCASH price index in USD via CoinGecko
