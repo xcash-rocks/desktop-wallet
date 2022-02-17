46a7c764de7460b966b5016497e9b2f7815fbd10
xcash-rocks <dev@xcash.rocks>
2022-02-17 11:47:15 +0100

    cleanup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
@@ -11,13 +11,13 @@ import { AuthComponent } from './theme/layout/auth/auth.component';
 import { NavigationComponent } from './theme/layout/admin/navigation/navigation.component';
 import { NavLogoComponent } from './theme/layout/admin/navigation/nav-logo/nav-logo.component';
 import { NavContentComponent } from './theme/layout/admin/navigation/nav-content/nav-content.component';
-import {NavigationItem} from './theme/layout/admin/navigation/navigation';
+import { NavigationItem } from './theme/layout/admin/navigation/navigation';
 import { NavGroupComponent } from './theme/layout/admin/navigation/nav-content/nav-group/nav-group.component';
 import { NavCollapseComponent } from './theme/layout/admin/navigation/nav-content/nav-collapse/nav-collapse.component';
 import { NavItemComponent } from './theme/layout/admin/navigation/nav-content/nav-item/nav-item.component';
 import { NavBarComponent } from './theme/layout/admin/nav-bar/nav-bar.component';
-import {ToggleFullScreenDirective} from './theme/shared/full-screen/toggle-full-screen';
-import {NgbButtonsModule, NgbDropdownModule, NgbTabsetModule, NgbTooltipModule} from '@ng-bootstrap/ng-bootstrap';
+import { ToggleFullScreenDirective } from './theme/shared/full-screen/toggle-full-screen';
+import { NgbButtonsModule, NgbDropdownModule, NgbTabsetModule, NgbTooltipModule } from '@ng-bootstrap/ng-bootstrap';
 import { NavLeftComponent } from './theme/layout/admin/nav-bar/nav-left/nav-left.component';
 import { NavRightComponent } from './theme/layout/admin/nav-bar/nav-right/nav-right.component';
 import { ConfigurationComponent } from './theme/layout/admin/configuration/configuration.component';
